cf8f2ea8abe2d0b8b6e70e21858ccbcbd06e4ac8
Daniel Arzanipour <daniel.arzanipour@gmail.com>
2021-12-03 08:53:06 -0500

    use inquirer to print out the html answers

--- apps.js
+++ apps.js
@@ -1,16 +1,9 @@
 const { prompt } = require("inquirer");
 const inquirer = require("inquirer");
 //^ Will come back to this code later
-// const fs = require("fs");
-// const generatePage = require(`./src/page-template.js`);
+const fs = require("fs");
+const generatePage = require(`./src/page-template.js`);
 
-// const pageHTML = generatePage(Name, Github);
-
-// // Data, Options, Callback-Function
-// fs.writeFile("index.html", pageHTML, (err) => {
-//   if (err) throw err;
-//   console.log("Portfolio complete! Check out index.html to see the output!");
-// });
 //^ Will come back to this code later
 
 const promptUser = () => {
@@ -159,5 +152,10 @@ const promptProject = (portfolioData) => {
 promptUser()
   .then(promptProject)
   .then((portfolioData) => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+
+    // Data, Options, Callback-Function
+    fs.writeFile("index.html", pageHTML, (err) => {
+      if (err) throw new Error(err);
+    });
   });
